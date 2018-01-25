5bda2a7b9014933b8543455a26ad55963ddda711
TimahTay <tjtatex@gmail.com>
2018-01-25 11:02:31 -0600

    Initial commit

---
+++ src/main/java/com/timdan/ftcscoutingapp/DataHandler.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.timdan.ftcscoutingapp;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Admin
+ */
+public class DataHandler {
+    
+    public static TreeMap<Integer, ArrayList<String>> importData(File file) {
+        
+        TreeMap<Integer, ArrayList<String>> matchData = new TreeMap();
+        
+        //TODO: IMPORT FROM EXCEL FILE DAN
+        
+        return matchData;
+    }
+    
+    
+}
